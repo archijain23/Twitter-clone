2ccce6965fde5d95fcb98ddb9fa99082a491fbf0
Archi Jain <jainarchi023@gmail.com>
2024-07-02 22:56:42 +0530

    fifth modification

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const port = process.env.PORT;
-const cors = require("cors");
+import dotenv from "dotenv";
+import express from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const app = express();
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-const path = require("path");
-const fileURLToPath = require("url").fileURLToPath;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //use the client app
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
