56cbc184677e0c1d4970b59fd555660cfdb718d6
Archi Jain <jainarchi023@gmail.com>
2024-07-18 22:22:15 +0530

    changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,62 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import commonjs from "@originjs/vite-plugin-commonjs";
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
   plugins: [
     react(),
-    commonjs(),
-    nodePolyfills({
-      include: ["process", "buffer", "util", "stream"],
+    viteCommonjs(),
+    inject({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
     }),
   ],
+  resolve: {
+    alias: {
+      util: "util/",
+      stream: "stream-browserify",
+      buffer: "buffer/",
+      process: "process/browser",
+    },
+  },
   define: {
     "process.env": {},
-    global: "window",
+    global: "globalThis",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
   },
   build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    rollupOptions: {
+      plugins: [
+        inject({
+          process: "process/browser",
+          Buffer: ["buffer", "Buffer"],
+        }),
+      ],
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://twitter-clone-xylb.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
 });
