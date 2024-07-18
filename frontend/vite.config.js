17246212ae6e70da92df452dd9b34cfc490757d4
Archi Jain <jainarchi023@gmail.com>
2024-07-18 21:37:11 +0530

    changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import inject from "@rollup/plugin-inject";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    inject({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
+    }),
+  ],
+  resolve: {
+    alias: {
+      // Polyfills for Node.js modules
+      util: "rollup-plugin-node-builtins",
+      stream: "rollup-plugin-node-builtins",
+      buffer: "rollup-plugin-node-globals",
+      process: "rollup-plugin-node-globals",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser global polyfills
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
   server: {
     proxy: {
       "/api": {
